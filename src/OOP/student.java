5b4f4dacf101b245c52775be69f1230eb2371bfb
VicellesJaylourdE <jayvicelles2003@gmail.com>
2023-11-30 11:07:38 +0800

    commit

--- src/OOP/student.java
+++ src/OOP/student.java
@@ -10,5 +10,42 @@ package OOP;
  * @author Admin
  */
 public class student {
+    String name;
+    String section;
+    String motto;
     
+  public String getName(){
+      return name;
+  }
+  public void setName(String name){
+      this.name=name;
+      
+  }
+    public String getSection(){
+      return section;
+  }
+    public void setSection(String section){
+    this.section=section;
+    }
+    public String getMotto(){
+    return motto;
+    }
+  public void setMotto(String motto){
+  this.motto=motto;
+  }
+  }
+
+class main {
+    
+public static void main(String[] args){
+
+    student s = new student();
+    
+s.setName("Jay Lourd E. Vicelles");
+s.setSection("IT24C");
+s.setMotto("Follow your heart and do what makes you happy");
+System.out.println("Name: " + s.getName());
+System.out.println("Section: " + s.getSection());
+System.out.println("Motto: " + s.getMotto());
+}
 }
